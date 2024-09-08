b51adc2bdbfb920d89b3869d8a6dbba1914591d8
FahdAliA <fahdaliansari@gmail.com>
2024-09-08 23:14:16 +0530

    generic view

--- seeds/views.py
+++ seeds/views.py
@@ -1,41 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework import status
+from seeds.models import Events, Startups, GovernBdy, About
+from seeds.serializers import EventsSerializer, StartupSerializer ,GovernBdySerializer, AboutSerializer
+from django.http import Http404
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from seeds.models import Events, Startups, GovernBdy
-from seeds.serializers import EventsSerializer, StartupSerializer ,GovernBdySerializer
+from rest_framework import generics
 
 
+class EventsView(generics.ListCreateAPIView):
 
-@api_view(['GET',])
-def EventsView(request):
+    serializer_class = EventsSerializer
     queryset = Events.objects.all()
-    serializer = EventsSerializer(queryset, many=True)
-    return Response(serializer.data)
 
-@api_view(['GET','POST',])
-def StartupsView(request):
-    if request.method == "GET":
-        queryset = Startups.objects.all()
-        serializer = StartupSerializer(queryset, many=True)
-        return Response(serializer.data)
-    if request.method == "POST":
-        serializer = StartupSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class GovernbdyView(generics.ListCreateAPIView):
 
-@api_view(['GET',])
-def GovernbdyView(request):
+    serializer_class = GovernBdySerializer
     queryset = GovernBdy.objects.all()
-    serializer = GovernBdySerializer(queryset, many=True)
-    return Response(serializer.data)
 
-@api_view(['GET',])
-def AboutView(request):
+
+class AboutView(generics.ListCreateAPIView):
+
+    serializer_class =  AboutSerializer
     queryset = About.objects.all()
-    serializer = AboutSerializer(queryset)
-    return Response(serializer.data)
+
+class StartupsView(generics.ListCreateAPIView):
+
+    serializer_class = StartupSerializer
+    queryset = Startups.objects.all()
